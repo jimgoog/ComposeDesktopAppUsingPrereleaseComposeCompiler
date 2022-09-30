4c72dff5e9c2e22b0f7b2a8c07ee61a99677104e
Jim Sproch <jsproch@google.com>
2022-09-30 10:44:32 -0700

    Remove unnecessary jvmTarget spec

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,6 @@ allprojects {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "1.8"
         freeCompilerArgs += listOf(
             "-P",
             "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
